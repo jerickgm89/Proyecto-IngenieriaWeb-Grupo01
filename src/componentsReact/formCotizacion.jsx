decd12b49f94bc52618eb98d794673a3c28fea1a
JErickDev <jerickgm89@gmail.com>
2023-09-24 10:39:05 -0500

    Branch para mejorar el menubar con material Tailwind

--- src/componentsReact/formCotizacion.jsx
+++ src/componentsReact/formCotizacion.jsx
@@ -1,3 +1,5 @@
+import { useFetch } from "../tools/useFetch";
+
 import {
     Input,
     Button,
@@ -7,15 +9,24 @@ import {
   } from "@material-tailwind/react";
 
 export default function formCotizacion(){
+
+    const URL = "http://18.233.143.65:3000/service/all";
+    const URL2 = "https://jsonplaceholder.typicode.com/users"
+
+    const { datos, loading } = useFetch(URL);
+
+      
+    console.log(datos?.data?.map((item) => item.description))
+
     return(
         <>
             <div className="sm:mx-10 md:mx-52 max-sm:px-10 align-middle items-center font-semibold pt-3">
                 <div>
                     <h2>1. INFORMACION PERSONAL</h2>
                     <div className="mb-4 flex flex-row gap-9 lg-max:flex-col pt-5">
-                        <Input size="lg" label="Name" />
-                        <Input size="lg" label="Telf" />
-                        <Input size="lg" label="Email" required />
+                        <Input size="lg" label="Nombre y Apellidos" />
+                        <Input size="lg" label="Telfono" />
+                        <Input size="lg" label="Correo Electronico" required />
                     </div>
                 </div>
                 <div>
@@ -25,10 +36,28 @@ export default function formCotizacion(){
                         <Input size="lg" label="Modelo del vehiculo" />
                         <Input size="lg" label="Numero de placa" />
                         <Input type="tel" size="lg" label="Año de fabricacion" />
-                        <Select label="Elige tu servicio">
-                            <Option>Cambio de aceite</Option>
-                            <Option>Mantenimiento Preventivo Mayor</Option>
-                            <Option>Mantenimiento Preventivo Menor</Option>
+                        <Select color="teal" label="Selecione el servicio">
+                            <Option>Cambios de aceite</Option>
+                            <Option>Mantenimiento preventivo Mayor</Option>
+                            <Option>Mantenimiento preventivo Menor</Option>
+                            <Option>Mecanica General</Option>
+                            <Option>Alineamiento y balanceo</Option>
+                            <Option>Reparacion del Sistema de Freno</Option>
+                            <Option>Cambio de pastillas</Option>
+                            <Option>Reparacion de Motor</Option>
+                            <Option>Direccion y suspension</Option>
+                            <Option>Kit de distribucion</Option>
+                            <Option>Kit de embrague</Option>
+                            <Option>Reparacion de Caja MT/AT</Option>
+                            <Option>Diagnostico de fallas</Option>
+                            <Option>Osciloscopio - escaner alta gama</Option>
+                            <Option>Planchado y Pintura</Option>
+                            <Option>Tratamiento de pintura</Option>
+                            <Option>Pintura en General</Option>
+                            <Option>Carga de aire acondicionado</Option>
+                            <Option>Undercoating y zincado</Option>
+                            <Option>Importacion de repuestos</Option>
+                            <Option>Lavado de Salon</Option>
                         </Select>
                     </div>
                 </div>
--- src/tools/useFetch.js
+++ src/tools/useFetch.js
@@ -1,16 +1,16 @@
 import { useEffect, useState } from "react";
 
 export function useFetch(url){
-    const [data, setData] = useState(null);
+    const [datos, setData] = useState([]);
     const [loading, setLoading] = useState(true);
     
     useEffect(()=>{
         setLoading(true);
         fetch(url)
             .then((response) => response.json())
-            .then((data) => setData(data))
+            .then((datos) => setData(datos))
             .finally(() => setLoading(false));
     },[]);
 
-    return { data };
+    return { datos, loading };
 }
